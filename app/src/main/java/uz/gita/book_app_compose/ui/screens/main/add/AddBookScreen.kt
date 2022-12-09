788d357b7191171fb4f3980fb0d078cd7ec17620
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-12-09 11:46:07 +0500

    #search bar changed

--- app/src/main/java/uz/gita/book_app_compose/ui/screens/main/add/AddBookScreen.kt
+++ app/src/main/java/uz/gita/book_app_compose/ui/screens/main/add/AddBookScreen.kt
@@ -1,5 +1,6 @@
 package uz.gita.book_app_compose.ui.screens.main.add
 
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.*
@@ -89,6 +90,7 @@ fun AddBookScreenContent(uiState: AddUiState, onEventDispatcher: (AddBookIntent)
                 .fillMaxWidth()
                 .height(56.dp)
         ) {
+            Log.d("TTT", "AddBookScreenContent: ")
             navigator.pop()
         }
         Spacer(
